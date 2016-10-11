174c873beaf33b4b5bf82a07a4b816df81a7d185
Nikolay Gavryliuk <gavrilyuk.nikolay@gmail.com>
2016-10-11 12:47:07 +0300

    remove urlpatterns to debug http responses like 400, 403, 404 and 500

--- {{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/config/urls.py
@@ -21,13 +21,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', default_views.bad_request),
-        url(r'^403/$', default_views.permission_denied),
-        url(r'^404/$', default_views.page_not_found),
-        url(r'^500/$', default_views.server_error),
-    ]
